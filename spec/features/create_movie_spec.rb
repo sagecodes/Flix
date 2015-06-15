1f47982d4756d8b359371057fa0faab569a99b0a
Sage Elliott <wildtroll@gmail.com>
2015-06-15 13:34:55 -0700

    add styling to copied from code bundle.

--- spec/features/create_movie_spec.rb
+++ spec/features/create_movie_spec.rb
@@ -14,6 +14,11 @@ describe "Creating a new movie" do
     fill_in "Total gross", with: "75000000"
     fill_in "Released on", with: (Time.now.year - 1).to_s#, :from => "movie_released_on_1i"
 
+    fill_in "Cast", with: "The award-winning cast"
+    fill_in "Director", with: "The ever-creative director"
+    fill_in "Duration", with: "123 min"
+    fill_in "Image file name", with: "movie.png"
+
     click_button 'Create Movie'
 
     expect(current_path).to eq(movie_path(Movie.last))
